ab7553caa11b212220b7808e48439f650f5e806b
Miguel Osinada <miguelosinada@gmail.com>
2025-02-19 07:58:23 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/mad_collaborative_osinada_peralta/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mad_collaborative_osinada_peralta
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mad_collaborative_osinada_peralta.ui.theme.MADCollaborativeOsinadaPeraltaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MADCollaborativeOsinadaPeraltaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MADCollaborativeOsinadaPeraltaTheme {
+        Greeting("Android")
+    }
+}
